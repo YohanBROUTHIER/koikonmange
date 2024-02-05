db6f74d61697c28205da9a0c3cea7c91eb505129
YohanBROUTHIER <yohan.brouthier@oclock.school>
2024-02-05 22:29:08 +0100

    add coreValidator

---
+++ src/helpers/validator/core.validator.js
@@ -0,0 +1,13 @@
+class CoreValidator {
+  static checkId(id) {
+    if ( id.match(/^[1-9]\d*$/) ) {
+      throw new ApiError("ID should be a valid integer", {httpStatus:400});
+    }
+  }
+
+  static checkIfExist(data, dataName) {
+    if (!data) {
+      throw new ApiError(`${dataName} not found.`, {httpStatus:404});
+    }
+  }
+}
