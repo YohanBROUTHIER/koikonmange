94e9de71d42220b97e83cc954345c47cf01062a3
Valentin Benard <valzer@MacBook-Air-de-Valentin.local>
2024-02-06 13:15:54 +0100

    history_validator-structure

---
+++ src/helpers/validator/history.validator.js
@@ -0,0 +1,20 @@
+import CoreValidator from "./core.validator.js";
+import ApiError from "../apiError.js";
+
+export default class HistoryValidator extends CoreValidator {
+  static checkBodyForCreate() {
+   
+  }
+  static checkBodyForUpdate() {
+
+   
+  }
+
+  static checkIfMailIsUsed() {
+    
+  }
+
+  static  checkUserSignin() {
+    
+  }
+}
