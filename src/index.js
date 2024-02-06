5fcd3746cf8665a288c3fffbe37d9c43022332ac
Yohan BROUTHIER <yohan.brouthier@oclock.school>
2024-02-06 10:15:49 +0100

    add express-session package

--- src/index.js
+++ src/index.js
@@ -4,31 +4,34 @@ import cors from "cors";
 
 // Import local dependencies
 import { router as apiRouter } from "./routers/index.js";
-import { bodySanitizer, errorMiddleware, notFoundMiddleware } from "./middlewares/index.js";
+import { bodySanitizer, errorMiddleware, notFoundMiddleware, sessionMiddleware } from "./middlewares/index.js";
 
 // Create Express App
 const app = express();
 
 // Allow some Cross origin requests
-app.use(cors({ origin: process.env.CORS })); // * = n'importe quel domaine ! A changer en production
+app.use(cors({ origin: process.env.CORS }));
 
-// Servir statiquement le dossier /dist pour que le client puisse récupérer le code client !
+app.use(sessionMiddleware.session); //Session parameters
+app.use(sessionMiddleware.sessionInit); //Add to res.locals
+
+// Statically serve the /dist folder
 app.use(express.static("./dist"));
 
 // Add body parser
-app.use(express.urlencoded({ extended: true })); // Ce body parser est capable de parser les body au format application/x-www-urlencoded (eg, les body envoyé par un <form> HTML)
-app.use(express.json()); // Ce body parser parse les body au format application/json
+app.use(express.urlencoded({ extended: true })); // Body parser for application/x-www-urlencoded
+app.use(express.json()); // Body parser for application/json
 
-// Generalisation du contrôle des inputs contre les potentielles injections XSS
+// XSS injection protection in req.body
 app.use(bodySanitizer);
 
-// Definit le router apiRouter pour les routes commencant par /api
+// Prefix api routes with "/api"
 app.use("/api", apiRouter);
 
 // Not Found Middleware (404)
 app.use(notFoundMiddleware);
 
-// gestionnaire centralisé d'erreurs
+// Centralized error manager
 app.use(errorMiddleware);
 
 export default app;
--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -2,3 +2,6 @@ export {default as notFoundMiddleware} from "./notFoundMiddleware.js";
 export {default as errorMiddleware} from "./errorMiddleware.js";
 export {default as errorHandler} from "./errorHandler.js";
 export {default as bodySanitizer} from "./body-sanitizer.js";
+export {default as isConnected} from "./isConnected.js";
+export {default as isDisconnected} from "./isDisconnected.js";
+export {default as sessionMiddleware} from "./sessionMiddleware.js";
