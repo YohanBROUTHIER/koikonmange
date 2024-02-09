33967538122f5b8df423b74c9baf41975207341c
Yohan BROUTHIER <yohan.brouthier@oclock.school>
2024-02-09 14:06:37 +0100

    repair script create_table

--- data/create_tables.sql
+++ data/create_tables.sql
@@ -1,21 +1,93 @@
 BEGIN;
 DROP FUNCTION IF EXISTS
-  create_user(json), find_user(), find_user(int), update_user(json), delete_user(json),
+  create_user(json), find_user(), find_user(int), update_user(json), delete_user(int),
   create_user_key(json), find_user_key(json), delete_user_key(json),
-  get_history(int), create_history(), update_history(), delete_history(),
-  get_recipe_history(),
+  get_history(int), create_history(json), delete_history(int),
+  get_valid_recipe_history(INT),
   create_family(json), find_family(), find_family(int), update_family(json), delete_family(int),
-  create_recipe(json), find_recipe(), find_recipe(int), update_recipe(json), delete_recipe(json);
+  create_recipe(json), find_recipe(), find_recipe(int), update_recipe(json), delete_recipe(int);
 
-DROP TABLE IF EXISTS "user", "user_key", "history", "family", "recipe" CASCADE;
+DROP TYPE IF EXISTS short_user, history_with_recipe, short_family, short_recype;
+
+DROP TABLE IF EXISTS "user", "user_key", "history", "history_has_recipe", "family", "recipe" CASCADE;
+
+--  ---------------------------------------- Family table -------------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS "family" (
+  "id" INT PRIMARY KEY,
+  "name" TEXT NOT NULL UNIQUE,
+  "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  "updated_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  "delete_at" TIMESTAMPTZ
+);
+
+--  ---------------------------------------- Family type -------------------------------------------------------
+
+CREATE TYPE short_family AS (
+	"id" int,
+  "name" text
+);
+
+--  ---------------------------------------- Family function -------------------------------------------------------
+
+CREATE FUNCTION create_family(json) RETURNS "family" AS $$
+  INSERT INTO "family"
+  ("name") VALUES (($1->>'name')::TEXT)
+  RETURNING *
+$$ LANGUAGE SQL;
+
+CREATE FUNCTION find_family() RETURNS SETOF short_family AS $$
+  SELECT "id","name" FROM "family"
+$$ LANGUAGE SQL;
+
+CREATE FUNCTION find_family(INT) RETURNS short_family AS $$
+  SELECT "id","name" FROM "family"
+  WHERE "id" = $1
+$$ LANGUAGE SQL;
+
+CREATE FUNCTION update_family(json) RETURNS "family" AS $$
+  UPDATE "family" SET (
+    "name",
+    "updated_at"
+  ) = (
+    COALESCE(($1->>'name')::TEXT, "name"),
+    now()
+  )
+  WHERE "id" = ($1->>'id')::INT
+  RETURNING *
+$$ LANGUAGE SQL;
+
+CREATE FUNCTION delete_family(INT) RETURNS "family" AS $$
+  UPDATE "family" SET "delete_at"= now()
+  WHERE "id" = $1
+  AND "delete_at" IS NULL
+	RETURNING *
+$$ LANGUAGE SQL;
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--  ---------------------------------------- User tables -------------------------------------------------------
 CREATE TABLE IF NOT EXISTS "user"(
   "id" int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   "name" text NOT NULL,
   "email" text NOT NULL,
   "password" text NOT NULL,
   "active" boolean NOT NULL DEFAULT false,
+  "is_admin" boolean NOT NULL DEFAULT false,
   "created_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
   "updated_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
   "delete_at" TIMESTAMPTZ
@@ -29,7 +101,19 @@ CREATE TABLE IF NOT EXISTS "user_key"(
   "updated_at" TIMESTAMPTZ NOT NULL DEFAULT now()
 );
 
-CREATE FUNCTION create_user(json) RETURNS "user" AS $$
+
+--  ---------------------------------------- User Type -------------------------------------------------------
+
+CREATE TYPE short_user AS (
+	"id" int,
+  "name" text,
+  "email" text,
+  "password" text,
+  "active" boolean,
+  "isAdmin" boolean
+);
+
+CREATE FUNCTION create_user(json) RETURNS short_user AS $$
 	INSERT INTO "user" (
 	  "name",
   	"email",
@@ -40,21 +124,21 @@ CREATE FUNCTION create_user(json) RETURNS "user" AS $$
     ($1->>'email')::text,
 	  ($1->>'password')::text
 	)
-	RETURNING * 	
+	RETURNING "id", "name", "email", "password", "active", "is_admin"
 $$ LANGUAGE sql;
 
-CREATE FUNCTION find_user() RETURNS SETOF "user" AS $$
-	SELECT * FROM "user"
+CREATE FUNCTION find_user() RETURNS SETOF short_user AS $$
+	SELECT "id", "name", "email", "password", "active", "is_admin" FROM "user"
   WHERE "delete_at" IS NULL
 $$ LANGUAGE sql;
 
-CREATE FUNCTION find_user(int) RETURNS "user" AS $$
-	SELECT * FROM "user"
+CREATE FUNCTION find_user(int) RETURNS short_user AS $$
+	SELECT "id", "name", "email", "password", "active", "is_admin" FROM "user"
   WHERE "id"=$1
   AND "delete_at" IS NULL
 $$ LANGUAGE sql;
 
-CREATE FUNCTION update_user(json) RETURNS "user" AS $$
+CREATE FUNCTION update_user(json) RETURNS short_user AS $$
 	UPDATE "user" SET (
 	  "name",
   	"email",
@@ -71,14 +155,14 @@ CREATE FUNCTION update_user(json) RETURNS "user" AS $$
 	)
   WHERE "id" = ($1->>'id')::int
   AND "delete_at" IS NULL
-	RETURNING * 	
+	RETURNING "id", "name", "email", "password", "active", "is_admin" 	
 $$ LANGUAGE sql;
 
-CREATE FUNCTION delete_user(json) RETURNS "user" AS $$
+CREATE FUNCTION delete_user(int) RETURNS short_user AS $$
 	UPDATE "user" SET "delete_at"	= now()
-  WHERE "id" = ($1->>'id')::int
+  WHERE "id" = $1
   AND "delete_at" IS NULL
-	RETURNING * 		
+	RETURNING "id", "name", "email", "password", "active", "is_admin" 		
 $$ LANGUAGE sql;
 
 CREATE FUNCTION create_user_key(json) RETURNS "user_key" AS $$
@@ -104,201 +188,179 @@ CREATE FUNCTION delete_user_key(json) RETURNS "user_key" AS $$
   RETURNING *
 $$ LANGUAGE sql;
 
-/* A COMPLETER, PAS TOTALEMENT SUR QU'ELLE SOIT TERMINEE */
-CREATE TABLE IF NOT EXISTS "history"(
-  "id" int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  "created_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
-  "updated_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
-  "user_id" int REFERENCES "user"("id") NOT NULL,
-  "delete_at" TIMESTAMPTZ
-);
 
-CREATE TABLE IF NOT EXISTS "recipe_has_history"(
-  "id"  int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  "validate" boolean NOT NULL, DEFAULT 'false',
-  "history_id" int REFERENCES "history"("id") NOT NULL,
-  "recipe_id" int REFERENCES "recipe"("id") NOT NULL,
-);
-
-CREATE FUNCTION get_history(INT) RETURNS SETOF "user" AS $$
-  SELECT * FROM "history"
-  WHERE "user_id" = $1
-$$ LANGUAGE SQL;
-
-CREATE FUNCTION update_history (json) RETURNS "history" AS $$
-	UPDATE "history" SET (
-  "updated_at"
-  
-	)
-  WHERE "id" = ($1->>'id')::int
-  AND "delete_at" IS NULL
-	RETURNING * 	
-$$ LANGUAGE sql;
-
-CREATE FUNCTION create_history (json) RETURNS "history" AS $$
-	INSERT INTO "history"  (
-    "user_id"
-    "created_at" TIMESTAMPTZ ,
-    "updated_at" TIMESTAMPTZ,
-	)
-  VALUES (
-    ($1->>'user_id')::int
-  )
-	RETURNING * 	
-$$ LANGUAGE sql;
+--  ---------------------------------------- Recipe table -------------------------------------------------------
 
 
-CREATE FUNCTION delete_history() RETURNS "history" AS $$
-	UPDATE "history" SET "delete_at"	= now()
-  WHERE "id" = ($1->>'id')::int
-  AND "delete_at" IS NULL
-	RETURNING * 		
-$$ LANGUAGE sql;
-
-
-CREATE FUNCTION get_recipe_history(
-    _recipe_id INT
-) RETURNS TABLE (
-    id INT,
-    validate BOOLEAN,
-    history_id INT
-) AS $$
-BEGIN
-    RETURN QUERY 
-    SELECT id, validate, history_id 
-    FROM recipe_has_history 
-    WHERE recipe_id = _recipe_id;
-END;
-$$ LANGUAGE SQL;
-
-
-
-
-
-
--- Creation of the "family" table
-CREATE TABLE IF NOT EXISTS "family" (
-  "id" INT PRIMARY KEY,
-  "name" TEXT NOT NULL,
-  "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-  "updated_at" TIMESTAMPTZ NOT NULL DEFAULT NOW()
-);
-
--- Function to create a family
-CREATE FUNCTION create_family(json) RETURNS "family" AS $$
-  INSERT INTO "family" ("id", "name")
-  VALUES (
-    ($1->>'id')::INT,
-    ($1->>'name')::TEXT
-  )
-  RETURNING *;
-$$ LANGUAGE SQL;
-
--- Function to find all families
-CREATE FUNCTION find_family() RETURNS SETOF "family" AS $$
-  SELECT * FROM "family";
-$$ LANGUAGE SQL;
-
--- Function to find a family by ID
-CREATE FUNCTION find_family(INT) RETURNS "family" AS $$
-  SELECT * FROM "family"
-  WHERE "id" = $1;
-$$ LANGUAGE SQL;
-
--- Function to update a family
-CREATE FUNCTION update_family(json) RETURNS "family" AS $$
-  UPDATE "family" SET
-    "name" = COALESCE(($1->>'name')::TEXT, "name"),
-    "updated_at" = NOW()
-  WHERE "id" = ($1->>'id')::INT
-  RETURNING *;
-$$ LANGUAGE SQL;
-
--- Function to delete a family
-CREATE FUNCTION delete_family(INT) RETURNS "family" AS $$
-  DELETE FROM "family"
-  WHERE "id" = $1
-  RETURNING *;
-$$ LANGUAGE SQL;
-
-
-
-
-
-
--- Create the "recipe" table
 CREATE TABLE IF NOT EXISTS "recipe"(
   "id" int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   "name" TEXT NOT NULL,
   "image" TEXT,
+  "steps" TEXT[] NOT NULL,
   "hunger" TEXT NOT NULL DEFAULT 'normal',
+  "time" INTERVAL NOT NULL,
+  "preparation_time" INTERVAL NOT NULL,
+  "user_id" INT NOT NULL REFERENCES "user"(id),
   "created_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
   "updated_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
-  "preparating_time" INT NOT NULL,
-  "user_id" INT NOT NULL REFERENCES "user"(id),
   "delete_at" TIMESTAMPTZ
 );
 
--- Create the create_recipe function
-CREATE FUNCTION create_recipe(json) RETURNS "recipe" AS $$
+--  ---------------------------------------- Recipe Type -------------------------------------------------------
+
+CREATE TYPE short_recype AS (
+	"id" int,
+  "name" text,
+  "image" TEXT,
+  "steps" TEXT[],
+  "hunger" TEXT,
+  "time" INTERVAL,
+  "preparation_time" INTERVAL,
+  "cooking-time" INTERVAL,
+  "userId" INT
+);
+
+--  ---------------------------------------- Recipe Function -------------------------------------------------------
+
+CREATE FUNCTION create_recipe(json) RETURNS "short_recype" AS $$
   INSERT INTO "recipe" (
     "name",
     "image",
     "hunger",
-    "preparating_time",
+    "steps",
+    "time",
+    "preparation_time",
     "user_id"
   )
   VALUES (
     ($1->>'name')::text,
     ($1->>'image')::text,
-    COALESCE(($1->>'hunger')::text, 'normal'),
-    ($1->>'preparating_time')::int,
+    ($1->>'hunger')::text,
+    ($1->>'steps')::TEXT[],
+    ($1->>'time')::INTERVAL,
+    ($1->>'preparation_time')::INTERVAL,
     ($1->>'user_id')::int
   )
-  RETURNING *   
+  RETURNING "id","name","image","steps","hunger","time","preparation_time",("time"-"preparation_time") AS "cooking-time","user_id"
 $$ LANGUAGE sql;
 
--- Create the find_recipe function without parameters
-CREATE FUNCTION find_recipe() RETURNS SETOF "recipe" AS $$
-  SELECT * FROM "recipe"
+CREATE FUNCTION find_recipe() RETURNS SETOF "short_recype" AS $$
+  SELECT "id","name","image","steps","hunger","time","preparation_time",("time"-"preparation_time") AS "cooking-time","user_id"
+  FROM "recipe"
   WHERE "delete_at" IS NULL
 $$ LANGUAGE sql;
 
--- Create the find_recipe function with an integer parameter
-CREATE FUNCTION find_recipe(int) RETURNS "recipe" AS $$
-  SELECT * FROM "recipe"
+CREATE FUNCTION find_recipe(int) RETURNS "short_recype" AS $$
+  SELECT "id","name","image","steps","hunger","time","preparation_time",("time"-"preparation_time") AS "cooking-time","user_id"
+  FROM "recipe"
   WHERE "id"=$1
   AND "delete_at" IS NULL
 $$ LANGUAGE sql;
 
--- Create the update_recipe function
-CREATE FUNCTION update_recipe(json) RETURNS "recipe" AS $$
+CREATE FUNCTION update_recipe(json) RETURNS "short_recype" AS $$
   UPDATE "recipe" SET (
     "name",
     "image",
     "hunger",
-    "preparating_time",
+    "steps",
+    "time",
+    "preparation_time",
     "updated_at"
   )
   = (
     COALESCE(($1->>'name')::text, "name"),
     COALESCE(($1->>'image')::text, "image"),
     COALESCE(($1->>'hunger')::text, "hunger"),
-    COALESCE(($1->>'preparating_time')::int, "preparating_time"),
+    COALESCE(($1->>'steps')::TEXT[], "steps"),
+    COALESCE(($1->>'time')::INTERVAL, "time"),
+    COALESCE(($1->>'preparation_time')::INTERVAL, "preparation_time"),
     now()
   )
   WHERE "id" = ($1->>'id')::int
   AND "delete_at" IS NULL
-  RETURNING *   
+  RETURNING "id","name","image","steps","hunger","time","preparation_time",("time"-"preparation_time") AS "cooking-time","user_id"   
 $$ LANGUAGE sql;
 
--- Create the delete_recipe function
-CREATE FUNCTION delete_recipe(json) RETURNS "recipe" AS $$
+CREATE FUNCTION delete_recipe(int) RETURNS "recipe" AS $$
   UPDATE "recipe" SET "delete_at" = now()
-  WHERE id = ($1->>'id')::int
+  WHERE id = $1
   AND delete_at IS NULL
   RETURNING *       
 $$ LANGUAGE sql;
+
+
+--  ---------------------------------------- History table -------------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS "history"(
+  "id" int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  "user_id" int REFERENCES "user"("id") NOT NULL,
+  "created_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
+  "updated_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
+  "delete_at" TIMESTAMPTZ
+);
+
+CREATE TABLE IF NOT EXISTS "history_has_recipe"(
+  "id"  int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  "validate" boolean NOT NULL DEFAULT 'false',
+  "history_id" int REFERENCES "history"("id") NOT NULL,
+  "recipe_id" int REFERENCES "recipe"("id") NOT NULL
+);
+
+--  ---------------------------------------- History type -------------------------------------------------------
+
+CREATE TYPE history_with_recipe AS (
+	"id" int,
+  "userId" int,
+  "date" TIMESTAMPTZ,
+  "recipes" json
+);
+
+--  ---------------------------------------- History function -------------------------------------------------------
+
+CREATE FUNCTION get_history(INT) RETURNS SETOF "history_with_recipe" AS $$
+  SELECT h."id",h."user_id",h."created_at",JSON_AGG(r.*) AS recipes FROM "history" AS h
+  LEFT JOIN "history_has_recipe" AS hhr
+  ON h."id" = hhr."history_id"
+  LEFT JOIN "recipe" AS r
+  ON hhr."recipe_id" = r."id"
+  WHERE h."user_id" = $1
+  GROUP BY h."id",h."user_id",h."created_at"
+  ORDER BY h."created_at" DESC
+$$ LANGUAGE SQL;
+
+CREATE FUNCTION create_history(json) RETURNS "history" AS $$
+	INSERT INTO "history"
+  ("user_id") VALUES (($1->>'user_id')::int)
+	RETURNING * 	
+$$ LANGUAGE sql;
+
+CREATE FUNCTION delete_history(INT) RETURNS "history" AS $$
+	UPDATE "history" SET "delete_at"= now()
+  WHERE "id" = $1
+  AND "delete_at" IS NULL
+	RETURNING * 		
+$$ LANGUAGE sql;
+
+CREATE FUNCTION get_valid_recipe_history(INT) RETURNS SETOF "history_with_recipe" AS $$
+	SELECT h."id",h."user_id",h."created_at",JSON_AGG(r.*) AS recipes FROM "history" AS h
+  LEFT JOIN "history_has_recipe" AS hhr
+  ON h."id" = hhr."history_id"
+  LEFT JOIN "recipe" AS r
+  ON hhr."recipe_id" = r."id"
+  WHERE h."user_id" = $1
+  AND hhr."validate" IS TRUE
+  GROUP BY h."id",h."user_id",h."created_at"
+  ORDER BY h."created_at" DESC	
+$$ LANGUAGE sql;
+
+
+
+
+
+
+
+
+
 
 
 COMMIT;
