1b4a95df4e073676bb1c2f30a1a91afb15d834f6
Yohan BROUTHIER <yohan.brouthier@oclock.school>
2024-02-12 13:58:21 +0100

    endpoints history in progress

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
 
 import { router as userRouter } from "./user.router.js";
-import { router as recipeRouter } from "./recipe.router.js";
-import { router as ingredientRouter } from "./ingredient.router.js";
+// import { router as recipeRouter } from "./recipe.router.js";
+// import { router as ingredientRouter } from "./ingredient.router.js";
 import { router as familyRouter } from "./family.router.js";
 import { router as historyRouter } from "./history.router.js";
 
@@ -11,7 +11,7 @@ export const router = Router();
 
 // Main API routes
 router.use(userRouter);
-router.use(recipeRouter);
-router.use(ingredientRouter);
+// router.use(recipeRouter);
+// router.use(ingredientRouter);
 router.use(familyRouter);
 router.use(historyRouter);
