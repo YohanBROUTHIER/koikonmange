27c0c78654c4de44b9c3c1655ec24d110b186ecf
Yohan BROUTHIER <yohan.brouthier@oclock.school>
2024-02-13 11:34:08 +0100

    add async to HistoryController

--- src/controllers/history.controller.js
+++ src/controllers/history.controller.js
@@ -15,14 +15,14 @@ export default class HistoryController extends CoreController{
     res.status(200).end();
   }
   
-  static updateRecipe(req, res) {
+  static async updateRecipe(req, res) {
     const data = this.validator.checkBodyForUpdateRecype(req.body);
 
     await this.datamapper.updateRecipeToHistory(data);
     res.status(200).end();
   }
 
-  static removeRecipe(req, res) {
+  static async removeRecipe(req, res) {
     const data = this.validator.checkBodyForRemoveRecype(req.body);
 
     await this.datamapper.removeRecipeToHistory(data);
